84898c0120d14676fb5db806e814184cb9cc20c5
upidapi <videw@icloud.com>
2022-09-27 15:09:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame as pg
+
+
+def serpinski_points(p1, p2, p3):
+    """
+      p3
+    p1  p2
+
+        p3
+      r2  r1
+    p1  r3  p2
+
+          p3
+        p1  p2
+      p3      p3
+    p1  p2  p1  p2
+    """
+
+    half_x = (p3[0] - p1[0]) / 2
+    half_y = (p3[1] - p1[1]) / 2
+
+    r1 = (p1[0] + half_x * 3, p1[1] - half_y)
+    r2 = (p1[0] + half_x * 1, p1[1] - half_y)
+    r3 = (p1[0] + half_x * 2, p1[1])
+
+    return r1, r2, r3
+
+
+def call_serpinski(p1, p2, p3):
+    
+
+
+pg.init()
+
+while True:
+    pass
